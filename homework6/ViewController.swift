1948619f70924a3d5ebb30458c9ce12933d1376a
Julija Klimenkova <146852581+JuliKlim@users.noreply.github.com>
2023-10-26 00:03:24 +0300

    corrected the task

--- homework6/ViewController.swift
+++ homework6/ViewController.swift
@@ -8,44 +8,71 @@
 import UIKit
 
 class Menu {
-    var dich = Dish()
-    init(dich: Dish) {
-        self.dich = dich
-    }
-}
-class Dish {
+    
+    //      или так я должна была сделать?
+    
+    //    var snacks = Snacks()
+    //    var manMenu = MainMenu()
+    //    var drinks = Drinks()
+    //    var desserts = Desserts()
+    //    init(snacks: Snacks = Snacks(), manMenu: MainMenu = MainMenu(), drinks: Drinks = Drinks(), desserts: Desserts = Desserts()) {
+    //        self.snacks = snacks
+    //        self.manMenu = manMenu
+    //        self.drinks = drinks
+    //        self.desserts = desserts
+    
     var name : String = ""
     var cost : Double = 0
+    var dich = Dish()
+    init(dich : Dish) {
+        self.dich = dich
+    }
+//
+//    func NameAndCost(_ dich: Dish) {
+//
+//    }
 }
 
-class Snacks : Dish { 
-    override init() {
-        super.init()
+class Snacks: Menu {
+    
     }
-}
-class MainMenu : Dish { 
-    override init() {
-        super.init()
+class MainMenu: Menu  {
+    
     }
-}
-class Drinks : Dish  { 
-    override init() {
-        super.init()
+class Drinks: Menu  {
+   
     }
-}
-class Desserts : Dish  { 
-    override init() {
-        super.init()
+class Desserts: Menu {
+   
     }
-}
+// правильно ли я выбрала абстрактный продукт? и здесь ли я должна создавать функцию?
+class Dish {
+       
+    }
 
-
+class Cold: Dish{
+}
+class Hot: Dish{
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let hot = Hot()
+        let cold = Cold()
+        
+        let soup =  MainMenu(dich: hot)
+        soup.cost = 10
+        soup.name = "Луковый суп"
+        print("\(soup.name) - \(soup.cost)")
+        
+        let lemonade = Drinks(dich: cold)
+        lemonade.cost = 2.35
+        lemonade.name = "Бамбл"
+        print("\(lemonade.name) - \(lemonade.cost)")
+        
     }
 
 
