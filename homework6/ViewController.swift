61982cedceaad922fdfe84e062f089e4dff2d10d
Julija Klimenkova <146852581+JuliKlim@users.noreply.github.com>
2023-10-28 17:05:21 +0300

    corrected task

--- homework6/ViewController.swift
+++ homework6/ViewController.swift
@@ -7,52 +7,47 @@
 
 import UIKit
 
+
 class Menu {
     
-    //      или так я должна была сделать?
-    
-    //    var snacks = Snacks()
-    //    var manMenu = MainMenu()
-    //    var drinks = Drinks()
-    //    var desserts = Desserts()
-    //    init(snacks: Snacks = Snacks(), manMenu: MainMenu = MainMenu(), drinks: Drinks = Drinks(), desserts: Desserts = Desserts()) {
-    //        self.snacks = snacks
-    //        self.manMenu = manMenu
-    //        self.drinks = drinks
-    //        self.desserts = desserts
-    
-    var name : String = ""
-    var cost : Double = 0
-    var dich = Dish()
-    init(dich : Dish) {
-        self.dich = dich
+    var snacks : Dish
+    var manMenu : Dish
+    var drinks : Dish
+    var desserts : Dish
+        init(snacks: Dish , manMenu: Dish, drinks: Dish, desserts: Dish) {
+            self.snacks = snacks
+            self.manMenu = manMenu
+            self.drinks = drinks
+            self.desserts = desserts
     }
-//
-//    func NameAndCost(_ dich: Dish) {
-//
-//    }
 }
 
-class Snacks: Menu {
-    
-    }
-class MainMenu: Menu  {
-    
-    }
-class Drinks: Menu  {
-   
-    }
-class Desserts: Menu {
-   
-    }
-// правильно ли я выбрала абстрактный продукт? и здесь ли я должна создавать функцию?
+
 class Dish {
-       
+    var name: String = ""
+    var cost: Double = 0.00
+    
+ 
     }
 
-class Cold: Dish{
-}
-class Hot: Dish{
+class OnionRings: Dish {
+    override init() {
+        super.init()
+        name = "Луковые кольца"
+        cost = 10.20
+    }
+    }
+    
+class Bamble: Dish {
+    override init() {
+        super.init()
+        name = "Бамбл"
+        cost = 6.00
+    }
+    }
+
+func price(_ dish: Dish) {
+    print ("\(dish.name) - \(dish.cost) руб.")
 }
 
 class ViewController: UIViewController {
@@ -60,21 +55,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let hot = Hot()
-        let cold = Cold()
+       let order = OnionRings()
+        let secondOrder = Bamble()
         
-        let soup =  MainMenu(dich: hot)
-        soup.cost = 10
-        soup.name = "Луковый суп"
-        print("\(soup.name) - \(soup.cost)")
-        
-        let lemonade = Drinks(dich: cold)
-        lemonade.cost = 2.35
-        lemonade.name = "Бамбл"
-        print("\(lemonade.name) - \(lemonade.cost)")
+       price(order)
+       price(secondOrder)
+     
+      
         
     }
 
 
 }
+
+func nameAndCost()  {
+
+    }
 
